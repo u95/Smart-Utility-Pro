df5b521b36bc2fba03467071a72194089543f73d
u95 <86998025+u95@users.noreply.github.com>
2026-07-22 08:48:12 +0530

    Create AdmobBannerAd.kt

---
+++ AdmobBannerAd.kt
@@ -0,0 +1,30 @@
+package com.umnministry.universaltv
+
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
+@Composable
+fun AdmobBannerAd(
+    adUnitId: String,
+    modifier: Modifier = Modifier
+) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            AdView(context).apply {
+                setAdSize(AdSize.BANNER)
+                this.adUnitId = adUnitId
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                loadAd(AdRequest.Builder().build())
+            }
+        }
+    )
+}
