135b469257db83afac0b1f5f3c1cbecaa63389b2
u95 <86998025+u95@users.noreply.github.com>
2026-07-27 12:13:56 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.smartutility.app',
+  appId: 'com.umnministry.smartutilitypro.app',
   appName: 'Smart Utility Pro',
   webDir: 'dist'
 };
